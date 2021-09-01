b2b2830048995d66a733b4e67b5c54bfc20e12ee
Vyshnavi Puniparthi <vyshupuniparthi@gmail.com>
2021-09-01 18:27:02 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.static(__dirname + '/dist/my1stngp'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/dist/my1stngp/index.html');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
